9746f10b31ff09357b6fafb3cfa4e9984c5da6f3
TWinstead-Lynn <twlynn1705@gmail.com>
2025-03-21 16:56:55 -0400

    Added some variation

--- main.ts
+++ main.ts
@@ -2,16 +2,46 @@ input.onButtonPressed(Button.A, function () {
     basic.showString("Who waits for the shadow")
 })
 input.onGesture(Gesture.Shake, function () {
-    basic.showString("" + (Adjectives[randint(0, 2)]))
-    basic.showString("" + (Nouns[randint(0, 2)]))
-    basic.showString("" + (Verbs[randint(0, 2)]))
-    basic.showString("" + (Adverbs[randint(0, 2)]))
+    basic.showString("" + (Adjectives[randint(0, 5)]))
+    basic.showString("" + (Nouns[randint(0, 5)]))
+    basic.showString("" + (Verbs[randint(0, 5)]))
+    basic.showString("" + (Adjectives[randint(0, 5)]))
+    basic.showString("" + (Nouns[randint(0, 5)]))
+    basic.showString("" + (Adverbs[randint(0, 5)]))
 })
 let Adverbs: string[] = []
 let Verbs: string[] = []
 let Nouns: string[] = []
 let Adjectives: string[] = []
-Adjectives = ["Tasty", "Wild", "Savory"]
-Nouns = ["Cat", "Hat", "NASA"]
-Verbs = ["Dances", "Throws", "Eats"]
-Adverbs = ["Quickly", "Wildly", "Ironically"]
+Adjectives = [
+"Tasty",
+"Wild",
+"Savory",
+"Bland",
+"Calm",
+"Sweet"
+]
+Nouns = [
+"Cat",
+"Hat",
+"NASA",
+"Dog",
+"Shoe",
+"Planet"
+]
+Verbs = [
+"Dances",
+"Throws",
+"Eats",
+"Sits",
+"Catches",
+"Drinks"
+]
+Adverbs = [
+"Quickly",
+"Wildly",
+"Ironically",
+"Slowly",
+"Calmly",
+"Logically"
+]
